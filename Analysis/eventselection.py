b2c8183d1f8695563d21f07e78e8c2919aa8d964
mollyhjorthjensen <mollyhjorthjensen@gmail.com>
2020-05-06 13:02:56 +0200

    Added calibration of vector signals

--- Analysis/eventselection.py
+++ Analysis/eventselection.py
@@ -1,4 +1,6 @@
 import sys
+import os
+import numpy as np
 import ROOT
 
 fileName = sys.argv[1]
@@ -7,7 +9,17 @@ assert(len(sys.argv) == 2)
  
 d = ROOT.ROOT.RDataFrame(treeName, fileName)
 
-d = d.Filter("(Sum(VecSignalScnt) > 0) || (Sum(VecSignalCkov) > 0)", "At least one signal")
+cal = np.load("calibration.pkl.npy", allow_pickle=True).item()
+print(cal)
+
+d = d.Filter("(Sum(VecSignalScnt) > 0) || (Sum(VecSignalCkov) > 0)", "at least one signal")
+d = d.Filter("Min(VecShowerEnergy) > 350.", "minimum shower energy 350 MeV")
+
+d = d.Define("VecSignalScnt_cal", f"VecSignalScnt*{cal['Scnt']}")
+d = d.Define("VecSignalCkov_cal", f"VecSignalCkov*{cal['Ckov']}")
+
+base, ext = os.path.splitext(fileName)
+d.Snapshot(treeName, base + "_filtered" + ext)
 
 print('All stats:')
 allCutsReport = d.Report()
