59f566515ebc8a1d6b423077d46b6b428fb3fa60
mollyhjorthjensen <mollyhjorthjensen@gmail.com>
2020-05-10 16:59:09 +0200

    WIP

--- Analysis/truthmatching.py
+++ Analysis/truthmatching.py
@@ -8,6 +8,9 @@ pdf_true = pdf_true[pdf_true.IsShower == 1]
 
 pdf_cluster = pd.read_csv('clustering.csv')
 
+# convert index to column
+pdf_cluster['clusterId'] = pdf_cluster.index
+
 pdf = pdf_true.merge(pdf_cluster, how='outer', on=['eventId'])
 
 # remember that the signals are pooled before clustering (hence the factor 2)
@@ -15,3 +18,24 @@ pdf['dist'] = pdf.apply(lambda x: np.sqrt((x.true_comi/2-x.cluster_comi)**2+(x.t
 
 print(pdf_true.shape, pdf_cluster.shape, pdf.shape)
 print(pdf.head(20))
+
+def func(x):
+	lst = []
+	while not x.empty:
+		df = x.loc[x.dist.idxmin()]
+		lst += [(df.clusterId.astype(int), df.showerId.astype(int))]
+		x = x[(x.showerId != df.showerId) & (x.clusterId != df.clusterId)]
+	return lst
+
+grouped = pdf[~pdf.clusterId.isna()].groupby('eventId').apply(func)
+print(grouped)
+
+flat_list = [item for sublist in grouped for item in sublist]
+df_new = pd.DataFrame(flat_list, columns=['clusterId', 'showerId'])
+pdf_cluster_new = pdf_cluster.merge(df_new, how='left', on=['clusterId'])
+print(pdf_cluster_new.head())
+
+
+pdf_final = pdf_true.merge(pdf_cluster_new, how='outer', on=['eventId', 'showerId'])
+
+print(pdf_final.head())
