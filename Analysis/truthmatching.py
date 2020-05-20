c066f9264642fc512cca95456d7fd21c613d2a5f
mollyhjorthjensen <mollyhjorthjensen@gmail.com>
2020-05-20 12:00:45 +0200

    Match both signals

--- Analysis/truthmatching.py
+++ Analysis/truthmatching.py
@@ -1,22 +1,20 @@
+import sys
+import os
 import ROOT
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-pdf_true = pd.read_csv('truth.csv')
-pdf_cluster = pd.read_csv('clustering.csv')
-
-# remove showers not entering calorimeter
-pdf_true = pdf_true[pdf_true.IsShower == 1]
-
-# convert index to column
-pdf_cluster['clusterId'] = pdf_cluster.index.astype(int)
-
-pdf = pdf_true.merge(pdf_cluster, how='outer', on=['eventId'])
+path = sys.argv[1]
+assert len(sys.argv) == 2
 
 # remember that the signals are pooled before clustering (hence the factor 2)
-euclidean_distance = lambda x: np.sqrt((x.true_comi/2-x.cluster_comi)**2+(x.true_comj/2-x.cluster_comj)**2)
-pdf['dist'] = pdf.apply(euclidean_distance, axis=1)
+def euclidean_distance(x, signal):
+	assert signal in ['S', 'C']
+	if signal == 'S':
+		return np.sqrt((x.VecShowerScntCoMi/2-x.S_comi)**2+(x.VecShowerScntCoMj/2-x.S_comj)**2)
+	elif signal == 'C':
+		return np.sqrt((x.VecShowerCkovCoMi/2-x.C_comi)**2+(x.VecShowerCkovCoMj/2-x.C_comj)**2)
 
 def matching(x):
 	lst = []
@@ -26,31 +24,64 @@ def matching(x):
 		x = x[(x.showerId != df.showerId) & (x.clusterId != df.clusterId)]
 	return lst
 
-grouped_matching = pdf[(~pdf.showerId.isna()) & (~pdf.clusterId.isna())].groupby('eventId').apply(matching)
+# import dataframes
+pdf_true = pd.read_csv(os.path.join(path, 'truth.csv'))
+pdf_cluster = {}
+pdf_final = pdf_true.copy()
+for signal in ['S', 'C']:
+	pdf_cluster[signal] = pd.read_csv(os.path.join(path, signal+'_cluster.csv'))
+	pdf_cluster[signal]['clusterId'] = pdf_cluster[signal].index.astype(int)
+	pdf_merged = pdf_true.merge(pdf_cluster[signal], how='outer', on=['eventId'])
+	pdf_merged = pdf_merged[(~pdf_merged.showerId.isna()) & (~pdf_merged.clusterId.isna())]
+	pdf_merged['dist'] = pdf_merged.apply(lambda x: euclidean_distance(x, signal), axis=1)
+	grouped_matched = pdf_merged.groupby('eventId').apply(matching)
+	flatlist = [item for sublist in grouped_matched for item in sublist]
+	pdf_new = pd.DataFrame(flatlist, columns=['clusterId', 'showerId', signal+'_dist'])
+	pdf_cluster[signal] = pdf_cluster[signal].merge(pdf_new, how='left', on=['clusterId'])
+	pdf_cluster[signal].rename(columns={'clusterId': signal+'_clusterId'}, inplace=True)
+	pdf_final = pdf_final.merge(pdf_cluster[signal], how='outer', on=['eventId', 'showerId'])
 
-flat_list = [item for sublist in grouped_matching for item in sublist]
-df_new = pd.DataFrame(flat_list, columns=['clusterId', 'showerId', 'dist'])
-pdf_cluster_new = pdf_cluster.merge(df_new, how='left', on=['clusterId'])
-
-pdf_final = pdf_true.merge(pdf_cluster_new, how='outer', on=['eventId', 'showerId'])
-pdf_final['rad_mean'] = pdf_final.apply(lambda x: (x.S_sum*x.S_rad_mean+x.C_sum*x.C_rad_mean)/(x.S_sum+x.C_sum), axis=1)
-pdf_final['dist_thresh'] = pdf_final.apply(lambda x: np.sqrt(x.rad_mean**2 + 1**2), axis=1)
 pdf_final['showerIdBool'] = ~pdf_final.showerId.isna()
-pdf_final['clusterIdBool'] = ~pdf_final.clusterId.isna() & ((pdf_final.dist < pdf_final.dist_thresh) | pdf_final.dist.isna())
-
-def distance2charged(x):
-	df = x.loc[x.IsCharged]
-	x['dist2charge'] = x.apply(lambda x: (568/2)*np.sqrt(2) if df.empty else np.sqrt((df.cluster_comi-x.cluster_comi)**2+(df.cluster_comj-x.cluster_comj)**2), axis=1)
-	return x
-
-grouped_charged = pdf_final[~pdf_final.clusterId.isna()].groupby('eventId').apply(distance2charged)
-grouped_charged = grouped_charged.filter(items=['eventId', 'clusterId', 'dist2charge'])
-pdf_final = pdf_final.merge(grouped_charged, how='left', on=['eventId', 'clusterId'])
-
-print(pdf_final.head())
+pdf_final['S_clusterIdBool'] = ~pdf_final.S_clusterId.isna() & ((pdf_final.S_dist < pdf_final.S_rad_mean) | pdf_final.S_dist.isna())
+pdf_final['C_clusterIdBool'] = ~pdf_final.C_clusterId.isna() & ((pdf_final.C_dist < pdf_final.C_rad_mean) | pdf_final.C_dist.isna())
+pdf_final['clusterIdBool'] = pdf_final.S_clusterIdBool | pdf_final.C_clusterIdBool
+pdf_final.reset_index(drop=True, inplace=True)
+pdf_final['clusterId'] = pdf_final.apply(lambda x: x.name if x.clusterIdBool else np.nan, axis=1)
 
 confusion_matrix = pd.crosstab(pdf_final.showerIdBool, pdf_final.clusterIdBool, rownames=['Actual'], colnames=['Predicted'])
 print(confusion_matrix)
+
+def weighted_comi(x):
+	if np.isnan(x.S_comi):
+		return x.C_comi
+	elif np.isnan(x.C_comi):
+		return x.S_comi
+	else:
+		return (x.S_sum*x.S_comi+x.C_sum*x.C_comi)/(x.S_sum+x.C_sum)
+
+def weighted_comj(x):
+	if np.isnan(x.S_comj):
+		return x.C_comj
+	elif np.isnan(x.C_comj):
+		return x.S_comj
+	else:
+		return (x.S_sum*x.S_comj+x.C_sum*x.C_comj)/(x.S_sum+x.C_sum)
+
+# remember there is only one charged track in any event
+def distance2charged(x):
+	df = x.loc[x.IsCharged]
+	a = (568/2)*np.sqrt(2)
+	x['dist2charge'] = x.apply(lambda x: a if df.empty else np.sqrt((df.comi-x.comi)**2+(df.comj-x.comj)**2), axis=1)
+	return x
+
+pdf_final['comi'] = pdf_final.apply(weighted_comi, axis=1)
+pdf_final['comj'] = pdf_final.apply(weighted_comj, axis=1)
+pdf_final.IsCharged = pdf_final.IsCharged == 1.0
+grouped_charged = pdf_final[pdf_final.clusterIdBool].groupby('eventId').apply(distance2charged)
+grouped_charged = grouped_charged.filter(items=['eventId', 'clusterId', 'dist2charge'])
+pdf_final = pdf_final.merge(grouped_charged, how='left', on=['eventId', 'clusterId'])
+#
+print(pdf_final.head())
 
 # get dataframe when there is both a shower and cluster
 pdf_ml = pdf_final[pdf_final.showerIdBool & pdf_final.clusterIdBool]
@@ -67,4 +98,4 @@ pdf_ml.clusterId = pdf_ml.clusterId.astype(int)
 pdf_ml.PrimaryDecayMode = pdf_ml.PrimaryDecayMode.astype(int)
 
 print(pdf_ml)
-pdf_ml.to_csv("data.csv", index=False)
+pdf_ml.to_csv(os.path.join(path, "data.csv"), index=False)
