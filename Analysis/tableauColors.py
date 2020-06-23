2f162041621c9da058303d76d9720298eb70e0e5
mollyhjorthjensen <mollyhjorthjensen@gmail.com>
2020-06-23 13:42:14 +0200

    Bug fix in orange

--- Analysis/tableauColors.py
+++ Analysis/tableauColors.py
@@ -2,7 +2,7 @@ from ROOT import TColor
 
 palette = {
 	'blue':		TColor(TColor.GetFreeColorIndex(),	31.,	119.,	180.	),
-	'orange':	TColor(TColor.GetFreeColorIndex(),	255.,	127.,	14.	),
+	'orange':	TColor(TColor.GetFreeColorIndex(),	255./255.,	127./255.,	14./255.	),
 	'green':	TColor(TColor.GetFreeColorIndex(),	44.,	160.,	44.	),
 	'red':		TColor(TColor.GetFreeColorIndex(),      214.,	39.,	40.	),
 	'purple':	TColor(TColor.GetFreeColorIndex(),      148.,	103.,	189.	),
@@ -16,3 +16,5 @@ palette = {
 palette['neutral'] = palette['grey']
 palette['e-'] = palette['blue']
 palette['pi-'] = palette['red']
+palette['mu-'] = palette['orange']
+palette['gamma'] = palette['green']
